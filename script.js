367dc7650e05cb2d8479d5d18fd9b5a9314ffd34
Attar Tohari <attarutur.234@gmail.com>
2025-11-25 22:23:38 +0700

    h5 edited

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //TOOGLE CLASS ACTIVE
+const navbarNav = document.querySelector
+('.navbar-nav');
+
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+};
+
+// Click Sembarang Tempat Menghilangkan Menu
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function (e) {
+    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+})
